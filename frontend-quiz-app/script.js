bf4223ef9a7850a74d2d418016d541746c303166
Srishti Chaudhary <srishticho@gmail.com>
2025-01-21 10:37:41 +0530

    show quiz screen on selection and correct category

--- frontend-quiz-app/script.js
+++ frontend-quiz-app/script.js
@@ -1,12 +1,39 @@
-const toggleInput = document.getElementById("toggle");
-const setBodyStyle = document.body.style;
+const subjects = document.querySelectorAll(".subjects button");
+const titleWidget = document.querySelector(".title-widget");
+const initialScreen = document.querySelector(".initial-screen");
+const quizScreen = document.querySelector(".quiz-screen");
+const header = document.querySelector("header");
 
-toggleInput.addEventListener("click", (e) => {
-    if (toggleInput.checked) {
-        setBodyStyle.backgroundColor = "#F4F6FA";
-        setBodyStyle.backgroundImage = 'url("/assets/images/pattern-background-mobile-light.svg")';
-    } else {
-        setBodyStyle.backgroundColor = "#313E51";
-        setBodyStyle.backgroundImage = 'url("/assets/images/pattern-background-mobile-dark.svg")';
-    }
+const showHeaderCat = (subject) => {
+  header.insertAdjacentHTML(
+    "afterbegin",
+    `
+        <div class="category" id="${subject}">
+        <div class="option">
+          <img src="/assets/images/icon-${
+            subject === "javascript" ? "js" : subject
+          }.svg" alt="${subject}" width="30" />
+        </div>
+        ${
+          subject === "html"
+            ? "HTML"
+            : subject === "css"
+            ? "CSS"
+            : subject === "javascript"
+            ? "JavaScript"
+            : subject
+        }
+      </div>
+        `
+  );
+};
+
+subjects.forEach((subject) => {
+  subject.addEventListener("click", (e) => {
+    // alert(e.target.alt || e.target.id);
+    titleWidget.style.display = "none";
+    initialScreen.style.display = "none";
+    quizScreen.style.display = "grid";
+    showHeaderCat(e.target.alt || e.target.id);
+  });
 });
