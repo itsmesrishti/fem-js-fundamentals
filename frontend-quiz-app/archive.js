14df9d40a2130c457cdb909098673e36f5d4beee
Srishti Chaudhary <srishticho@gmail.com>
2025-01-31 09:19:50 +0530

    worked on adding functionality

---
+++ frontend-quiz-app/archive.js
@@ -0,0 +1,222 @@
+const subjects = document.querySelectorAll(".subjects button");
+const titleWidget = document.querySelector(".title-widget");
+const initialScreen = document.querySelector(".initial-screen");
+const quizScreen = document.querySelector(".quiz-screen");
+const resultScreen = document.querySelector(".result-screen");
+const header = document.querySelector("header");
+const quesNum = document.getElementById("ques-num");
+const question = document.getElementById("question");
+const ansGroup = document.querySelector(".ans-group");
+const options = document.querySelectorAll(".ans-group .category");
+const optionsOnly = document.querySelectorAll(".ans-group input");
+const optionsContent = document.querySelectorAll(".ans-group .option-text");
+const submitAnsBtn = document.querySelector(".ans-group button");
+const scoreEle = document.getElementById("score");
+const headingEle = document.querySelector("h1");
+const subheadingEle = document.querySelector(".title-widget p");
+const playAgainBtn = document.getElementById("play-again");
+
+let htmlQuiz = [];
+let cssQuiz = [];
+let jsQuiz = [];
+let accQuiz = [];
+let score = 0;
+let q = 0;
+let nextBtn = undefined;
+let isAnswerSubmitted = false;
+let selectedAns = "";
+
+fetch("data.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    htmlQuiz = data["quizzes"][0]["questions"];
+    cssQuiz = data["quizzes"][1]["questions"];
+    jsQuiz = data["quizzes"][2]["questions"];
+    accQuiz = data["quizzes"][3]["questions"];
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const showHeaderCat = (subject) => {
+  header.insertAdjacentHTML(
+    "afterbegin",
+    `
+        <div class="category" id="${subject}">
+        <div class="option">
+          <img src="/assets/images/icon-${
+            subject === "javascript" ? "js" : subject
+          }.svg" alt="${subject}" width="30" />
+        </div>
+        ${
+          subject === "html"
+            ? "HTML"
+            : subject === "css"
+            ? "CSS"
+            : subject === "javascript"
+            ? "JavaScript"
+            : subject
+        }
+      </div>
+        `
+  );
+};
+
+const setQuiz = (quizSub) => {
+  quesNum.innerText = q + 1;
+
+  question.innerText = quizSub[q]["question"];
+
+  let o = 0;
+
+  optionsContent.forEach((optionContent) => {
+    optionContent.innerText = quizSub[q]["options"][o];
+    o++;
+  });
+};
+
+const setSubject = (subject) => {
+  if (subject === "html") {
+    setQuiz(htmlQuiz);
+  }
+};
+
+const checkAnswer = (selectedAns) => {
+  if (selectedAns === htmlQuiz[q]["answer"]) {
+    console.log(score);
+    score += 1;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const disableOptions = () => {
+  options.forEach((option) => {
+    option.firstChild.setAttribute("disabled", "disabled");
+    option.style.cursor = "default";
+  });
+};
+
+const enableOptions = () => {
+  options.forEach((option) => {
+    option.firstChild.removeAttribute("disabled");
+    option.style.cursor = "pointer";
+  });
+};
+
+const reset = () => {
+  enableOptions();
+
+  // reset options
+  optionsContent.forEach((optionContent) => {
+    optionContent.innerText = "";
+  });
+
+  // remove answer state class from category
+  options.forEach((option) => {
+    if (option.firstChild.checked) {
+      option.className = "category";
+
+      // remove default checked state
+      // when moving between questions
+      option.firstChild.checked = false;
+    }
+  });
+};
+
+const getUserAnswer = () => {
+  options.forEach((option) => {
+    if (option.firstChild.checked) {
+      selectedAns = option.lastChild.innerText;
+    }
+  });
+
+  return selectedAns;
+};
+
+const applyAnswerStyle = (selectedAns) => {
+  if (checkAnswer(selectedAns)) {
+    options.forEach((option) => {
+      if (option.firstChild.checked) {
+        option.classList.add("correct-ans-selected");
+        option.classList.add("correct-ans");
+      }
+    });
+  } else {
+    options.forEach((option) => {
+      if (option.firstChild.checked) {
+        option.classList.add("wrong-ans-selected");
+      }
+      if (option.innerText === htmlQuiz[0]["answer"]) {
+        option.classList.add("correct-ans");
+      }
+    });
+  }
+};
+
+const checkOptionSelected = () => {
+  optionsOnly.forEach(option => {
+    if (option.checked) {
+      console.log("checked")
+      return true;
+    } else {
+      console.log("not checked");
+      return false;
+    }
+  })
+}
+
+const submitBtnListener = () => {
+  submitAnsBtn.addEventListener("click", () => {
+   
+      if (isAnswerSubmitted) {
+        submitAnsBtn.innerText = "Submit Answer";
+        enableOptions();
+        reset();
+        setQuiz(htmlQuiz);
+        isAnswerSubmitted = false;
+      } else {
+        selectedAns = getUserAnswer();
+        applyAnswerStyle(selectedAns);
+        disableOptions();
+        if (q < 9) {
+          submitAnsBtn.innerText = "Next Question";
+          isAnswerSubmitted = true;
+          q++;
+        } else {
+          titleWidget.style.display = "block";
+          resultScreen.style.display = "block";
+          scoreEle.innerText = score;
+          quizScreen.style.display = "none";
+          headingEle.innerHTML = `Quiz completed<span>You scored...</span>`;
+          subheadingEle.style.display = "none";
+        }
+      }
+    
+  });
+};
+
+submitBtnListener();
+
+subjects.forEach((subject) => {
+  subject.addEventListener("click", (e) => {
+    titleWidget.style.display = "none";
+    initialScreen.style.display = "none";
+    quizScreen.style.display = "grid";
+    showHeaderCat(e.target.alt || e.target.id);
+    setSubject(e.target.alt || e.target.id);
+  });
+});
+
+headingEle.innerHTML = `Welcome to the<span>Frontend Quiz!</span>`;
+
+playAgainBtn.addEventListener("click", () => {
+  initialScreen.style.display = "block";
+  resultScreen.style.display = "none";
+  headingEle.innerHTML = `Welcome to the<span>Frontend Quiz!</span>`;
+  score = 0;
+  q = 0;
+})
--- frontend-quiz-app/script.js
+++ frontend-quiz-app/script.js
@@ -2,38 +2,112 @@ const subjects = document.querySelectorAll(".subjects button");
 const titleWidget = document.querySelector(".title-widget");
 const initialScreen = document.querySelector(".initial-screen");
 const quizScreen = document.querySelector(".quiz-screen");
-const header = document.querySelector("header");
+const headerCat = document.querySelector("header .category");
+const quesNumEle = document.getElementById("ques-num");
+const questionEle = document.getElementById("question");
+const optionsTextEle = document.querySelectorAll(".option-text");
+const submitAnsBtn = document.querySelector(".ans-group button");
+const inputOptions = document.querySelectorAll(".ans-group input");
 
-const showHeaderCat = (subject) => {
-  header.insertAdjacentHTML(
-    "afterbegin",
-    `
-        <div class="category" id="${subject}">
-        <div class="option">
-          <img src="/assets/images/icon-${
-            subject === "javascript" ? "js" : subject
-          }.svg" alt="${subject}" width="30" />
-        </div>
-        ${
-          subject === "html"
-            ? "HTML"
-            : subject === "css"
-            ? "CSS"
-            : subject === "javascript"
-            ? "JavaScript"
-            : subject
-        }
-      </div>
-        `
-  );
+let quizzes = [];
+let subject = [];
+let questions = [];
+let quesNum = 0;
+let options = [];
+let selectedAns = "";
+
+fetch("data.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    quizzes = data["quizzes"];
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const extractQuiz = (subSelected) => {
+  if (subSelected === "html") {
+    subject = quizzes[0];
+  } else if (subSelected === "css") {
+    subject = quizzes[1];
+  } else if (subSelected === "javascript") {
+    subject = quizzes[2];
+  } else {
+    subject = quizzes[3];
+  }
+
+  questions = subject["questions"];
 };
 
-subjects.forEach((subject) => {
-  subject.addEventListener("click", (e) => {
-    // alert(e.target.alt || e.target.id);
-    titleWidget.style.display = "none";
-    initialScreen.style.display = "none";
-    quizScreen.style.display = "grid";
-    showHeaderCat(e.target.alt || e.target.id);
+const showQuizScreen = () => {
+  titleWidget.style.display = "none";
+  initialScreen.style.display = "none";
+  quizScreen.style.display = "grid";
+};
+
+const showHeaderCat = () => {
+  let subTitle = subject["title"].toLowerCase();
+
+  headerCat.style.display = "flex";
+  headerCat.insertAdjacentText("beforeend", subject["title"]);
+  headerCat.id = subTitle;
+  headerCat.querySelector("img").src = subject["icon"];
+  headerCat.querySelector("img").alt = subTitle;
+};
+
+const showQuestion = () => {
+  quesNumEle.innerText = quesNum + 1;
+  questionEle.innerText = questions[quesNum]["question"];
+  options = questions[quesNum]["options"];
+};
+
+const showOptions = () => {
+  let optionNum = 0;
+
+  optionsTextEle.forEach((optionTextEle) => {
+    optionTextEle.innerText = options[optionNum];
+    optionNum += 1;
   });
+};
+
+const isOptionSelected = () => {
+  let optionSelected = Array.from(inputOptions).find(
+    (inputOption) => inputOption.checked
+  );
+
+  selectedAns = optionSelected.parentElement.lastChild.innerText;
+
+  return optionSelected ? true : false;
+};
+
+const checkAnswer = (selectedAns) => {
+  if (selectedAns === questions[quesNum]["answer"]) {
+    alert("true");
+    return true;
+  } else {
+    alert("false");
+    return false;
+  }
+};
+
+subjects.forEach((sub) => {
+  sub.addEventListener("click", (e) => {
+    subSelected = e.target.id || e.target.alt;
+    showQuizScreen();
+    extractQuiz(subSelected);
+    showHeaderCat();
+    showQuestion();
+    showOptions();
+  });
+});
+
+submitAnsBtn.addEventListener("click", () => {
+  if (isOptionSelected()) {
+    checkAnswer(selectedAns);
+  } else {
+    alert("please select an option");
+  }
+  isOptionSelected();
 });
